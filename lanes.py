b2bb257de93446f83084d9180b8803eed4730b02
salmaramzy1 <104319142+salmaramzy1@users.noreply.github.com>
2022-04-24 21:15:47 +0200

    Create lanes.py

---
+++ lanes.py
@@ -0,0 +1,7 @@
+import cv2
+import nympy as np
+image = cv2.imread ('test_image.jpg')
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+cv2.imshow("result" , image)
+cv2.waitKey(0)
